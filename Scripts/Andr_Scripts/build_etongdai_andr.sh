9ca0566480838c416774a10953eaf6219599fdb0
罗斯(刘硕) <liushuo@glzc.com>
2017-11-30 09:28:24 +0800

    single channel package

--- Scripts/Andr_Scripts/build_etongdai_andr.sh
+++ Scripts/Andr_Scripts/build_etongdai_andr.sh
@@ -69,16 +69,7 @@ setconfig() {
 
 package() {
 	# $1-appPkg $2-appName $3-appVersion $4-appVersionCode	$5-channelName $6-channelId	$7-environment	$8-buildType $9-author
-	# appPkg=$1
-	# appName=$2
-	# appVersion=$3
-	# appVersionCode=$4
-	# channelName=$5
-	# channelId=$6
-	# environment=$7
-	# buildType=$8
-	# author=$9
-	# isChannels=${10}
+	# appPkg=$1 appName=$2 appVersion=$3 appVersionCode=$4 channelName=$5 channelId=$6 environment=$7 buildType=$8 author=$9 isChannels=${10}
 	
 	#clean proj
 	INFO "打包参数列表: appPkg:$appPkg, appName:$appName, appVersion:$appVersion, appVersionCode:$appVersionCode, channelName:$channelName, channelId:$channelId, environment:$environment, buildType:$buildType, author:$author, isChannels:$isChannels"
@@ -225,6 +216,109 @@ package() {
         		ERROR "upload apk to nexus fail."
     		fi
     	else
+    		channelId=`jq -r --arg channelName "$channelName" '.[] | select( .name | contains($channelName) ) | .value' $jFile`
+    		if [[ "$channelName" == "乐视" ]] || [[ "$channelName" == "应用宝应用市场" ]]; then
+		 	 	INFO "replace leshi or yyb lib"
+		 	 	INFO "remove useless dir and libdu.so"
+		 	 	[[ -f ${libDir}/armeabi/libdu.so ]] && `rm -f "${libDir}/armeabi/libdu.so"`
+		 	 	[[ -d ${libDir}/armeabi-v7a ]] && `rm -rf ${libDir}/armeabi-v7a`
+		 	 	[[ -d ${libDir}/x86 ]] && `rm -rf ${libDir}/x86`
+		 	 	if [[ -f ${libDir}/armeabi/libac.so ]]; then
+		 	 		INFO "libac.so already existed"
+		 	 		[[ -f ${libDir}/armeabi/libac.so ]] && `chmod 776 ${libDir}/armeabi/libac.so`
+		 	 		if [[ $? -ne 0 ]]; then
+		 	 			ERROR "chmod file fail"
+		 	 			exit 1
+		 	 		else
+		 	 			INFO "打包开始："	
+		 	 			timeBegin=`date '+%s'`
+		 	 			gradle -PAPPLICATION_ID="$appPkg" -PAPP_NAME="$appName" -PAPP_VERSION_NAME="$appVersion" -PAPP_VERSION_CODE="$appVersionCode" -PBUILD_SCRIPT="${PROJDIR}/build.gradle" \
+						-PUMENG_VALUE="$channelName" -PFRINED_ID="$channelId" -PENVIRONMENT="$environment" -PLABLE="$author" "assemble${buildType}" -b "${PROJDIR}/build.gradle" --stacktrace
+						if [ $? -ne 0 ]; then
+        					ERROR "编译失败！"
+        					exit 1
+    					else
+        					INFO "编译成功！耗时$(TIMECONSUMED)秒,位于：${BUILD_DIR}"
+    					fi
+    				fi
+    				INFO "move apk"
+					find ${BUILD_DIR} -d 1 -name '*.apk' -exec mv {} ./apks/${productName}.apk \;
+		 	 	else
+		 	 		INFO "libac.so is not existed"
+		 	 		INFO "cp libac.so"
+		 	 		cp -f ac/armeabi/libac.so ${libDir}/armeabi
+		 	 		[[ -f ${libDir}/armeabi/libac.so ]] && `chmod 776 ${libDir}/armeabi/libac.so`
+		 	 		if [[ $? -ne 0 ]]; then
+		 	 			ERROR "chmod file fail"
+		 	 			exit 1
+		 	 		else	
+		 	 			INFO "打包开始："	
+		 	 			timeBegin=`date '+%s'`
+						gradle -PAPPLICATION_ID="$appPkg" -PAPP_NAME="$appName" -PAPP_VERSION_NAME="$appVersion" -PAPP_VERSION_CODE="$appVersionCode" -PBUILD_SCRIPT="${PROJDIR}/build.gradle" \
+						-PUMENG_VALUE="$channelName" -PFRINED_ID="$channelId" -PENVIRONMENT="$environment" -PLABLE="$author" "assemble${buildType}" -b "${PROJDIR}/build.gradle" --stacktrace
+						if [ $? -ne 0 ]; then
+        					ERROR "编译失败！"
+        					exit 1
+    					else
+        					INFO "编译成功！耗时$(TIMECONSUMED)秒,位于：${BUILD_DIR}"
+    					fi
+    				fi
+					INFO "move apk"
+					find ${BUILD_DIR} -d 1 -name '*.apk' -exec mv {} ./apks/${productName}.apk \;
+		 	 	fi
+		 		
+		 	 else 
+		 	 	INFO "replace other channel lib"
+		 	 	INFO "remove libac.so"
+		 	 	[[ -f ${libDir}/armeabi/libac.so ]] && `rm -f "${libDir}/armeabi/libac.so"`
+		 	 	if [[ -f ${libDir}/armeabi/libdu.so ]]; then
+		 	 		INFO "libdu.so already existed"
+		 	 		for p in "armeabi" "armeabi-v7a" "x86" ; do
+		 	 			chmod 776 ${libDir}/${p}/libdu.so
+		 	 		done
+		 	 		if [[ $? -ne 0 ]]; then
+		 	 			ERROR "chmod file fail"
+		 	 			exit 1
+		 	 		else
+		 	 			INFO "打包开始："	
+		 	 			timeBegin=`date '+%s'`
+						gradle -PAPPLICATION_ID="$appPkg" -PAPP_NAME="$appName" -PAPP_VERSION_NAME="$appVersion" -PAPP_VERSION_CODE="$appVersionCode" -PBUILD_SCRIPT="${PROJDIR}/build.gradle" \
+						-PUMENG_VALUE="$channelName" -PFRINED_ID="$channelId" -PENVIRONMENT="$environment" -PLABLE="$author" "assemble${buildType}" -b "${PROJDIR}/build.gradle" --stacktrace
+						if [ $? -ne 0 ]; then
+        					ERROR "编译失败！"
+        					exit 1
+    					else
+        					INFO "编译成功！耗时$(TIMECONSUMED)秒,位于：${BUILD_DIR}"
+    					fi
+    				INFO "move apk"
+					find ${BUILD_DIR} -d 1 -name '*.apk' -exec mv {} ./apks/${productName}.apk \;	
+    				fi
+		 	 	else
+		 	 		INFO "libdu.so is not existed"
+		 	 		for p in "armeabi" "armeabi-v7a" "x86" ; do
+		 	 			INFO ">>>>>>cp $p"
+		 	 			cp -rf shumeng/${p} ${libDir}
+		 	 			[[ -f ${libDir}/${p}/libdu.so ]] && `chmod 776 ${libDir}/${p}/libdu.so`
+		 	 		done
+		 	 		if [[ $? -ne 0 ]]; then
+		 	 			ERROR "chmod file fail"
+		 	 			exit 1
+		 	 		else
+		 	 			INFO "打包开始："	
+		 	 			timeBegin=`date '+%s'`
+						gradle -PAPPLICATION_ID="$appPkg" -PAPP_NAME="$appName" -PAPP_VERSION_NAME="$appVersion" -PAPP_VERSION_CODE="$appVersionCode" -PBUILD_SCRIPT="${PROJDIR}/build.gradle" \
+						-PUMENG_VALUE="$channelName" -PFRINED_ID="$channelId" -PENVIRONMENT="$environment" -PLABLE="$author" "assemble${buildType}" -b "${PROJDIR}/build.gradle" --stacktrace
+						if [ $? -ne 0 ]; then
+        					ERROR "编译失败！"
+        					exit 1
+    					else
+        					INFO "编译成功！耗时$(TIMECONSUMED)秒,位于：${BUILD_DIR}"
+    					fi
+		 	 		fi
+		 	 		INFO "move apk"
+					find ${BUILD_DIR} -d 1 -name '*.apk' -exec mv {} ./apks/${productName}.apk \;
+		 	 	fi
+		 	 fi
     		INFO ">>>>>>$appPkg"
 			INFO "打包开始："	
 		 	timeBegin=`date '+%s'`
@@ -247,13 +341,8 @@ package() {
     		else
         		ERROR "upload apk to nexus fail."
     		fi
-
 		fi
-
 	fi	
-
-	# gradle -PAPPLICATION_ID="$appPkg" -PAPP_NAME="$appName" -PAPP_VERSION_NAME="$appVersion" -PAPP_VERSION_CODE="$appVersionCode" \
-	# -PUMENG_VALUE="$channelName" -PFRINED_ID="$channelId" -PENVIRONMENT="$environment" -PLABLE="$author" clean "assemble${buildType}" -b "${WORK_DIR}/android/app/build.gradle" --stacktrace
 
 }
 
