2b7b021d185012c90d0ecf4dc7401ab433a87d14
罗斯(刘硕) <fastcross@163.com>
2017-12-21 15:29:45 +0800

    add swith env mode

--- Scripts/iOS_Scripts/build_etongdai.sh
+++ Scripts/iOS_Scripts/build_etongdai.sh
@@ -258,7 +258,7 @@ case "$build_product" in
     "eTongDai")
             RESET
             setconfig
-            package "eTongDai" "eTongDai" "Release" "$DEVELOPER_SIGN_NAME_SYSH" "$PROVISIONING_PROFILE_SPECIFIER_SYSH_DEV_AUTO" "Automatic"
+            package "eTongDai" "eTongDai" "${buildType}" "$DEVELOPER_SIGN_NAME_SYSH" "$PROVISIONING_PROFILE_SPECIFIER_SYSH_DEV_AUTO" "Automatic"
             ;;
 esac
 
